bd47570e8deda8956243b47d70ecca847c68f2fc
Bogdan Birau <birau.bogdan92@gmail.com>
2021-10-11 13:37:40 +0100

    Working on the footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import TopNavbar from './components/TopNavbar'
+import BottomFooter from './components/BottomFooter'
+
 
 
 function App() {
   return (
     <div >
       <TopNavbar />
+      <BottomFooter />
     </div>
   );
 }
