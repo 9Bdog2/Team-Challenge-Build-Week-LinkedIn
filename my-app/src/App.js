577a72574503578e82c8087c814f2c611b370b38
Bogdan Birau <birau.bogdan92@gmail.com>
2021-10-11 11:27:52 +0100

    Top-Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import TopNavbar from './components/TopNavbar'
 
 
 function App() {
   return (
     <div >
-      
+      <TopNavbar />
     </div>
   );
 }
 
+
 export default App;
+
+
