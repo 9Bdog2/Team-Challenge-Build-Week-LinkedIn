ee00702eb35218611cd685fb9dacdfff1814c3ba
guillermoFragachan <guillefragachan@gmail.com>
2021-10-12 01:45:32 +0200

    fix styles

--- my-app/src/components/TopNavbar.jsx
+++ my-app/src/components/TopNavbar.jsx
@@ -3,11 +3,14 @@ import { Navbar, Nav, NavDropdown, Form, FormControl } from "react-bootstrap";
 import linkedIn_logo_initials from "../assets/linkedIn_logo_initials.png";
 import small_profile_picture from "../assets/small_profile_picture.jpg";
 import "./TopNavbar.css";
+import Container from 'react-bootstrap/Container'
 
 function TopNavbar() {
   return (
     <>
-      <Navbar bg="light" expand="lg">
+      <Navbar className='NavbarSizing' bg="light" expand="lg">
+
+        <Container>
         <Navbar.Brand href="#home">
           <img
             src={linkedIn_logo_initials}
@@ -22,8 +25,8 @@ function TopNavbar() {
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
         </Form>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" >
-          <Nav className="ml-auto">
+        <Navbar.Collapse id="basic-navbar-nav" className='iconsNavbar' >
+          <Nav className="ml-auto iconsNavbar" >
             <div>
               <div className="top_icons_container">
                 <Icon.HouseDoorFill className="top_icons" />
@@ -80,6 +83,9 @@ function TopNavbar() {
             </div>
           </Nav>
         </Navbar.Collapse>
+
+
+        </Container>
       </Navbar>
     </>
   );
