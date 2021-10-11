8d95e485c85dae47db208db21187a9dc9b5f99fa
guillermoFragachan <guillefragachan@gmail.com>
2021-10-11 10:37:19 +0200

    sidebar layout

---
+++ my-app/src/components/SideBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import PeopleViewed from './SideBarComponents/PeopleViewed' 
+
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <>
+        <PeopleViewed/>
+
+
+
+      </>
+    );
+  }
+}
+
+export default Sidebar;
---
+++ my-app/src/components/SideBarComponents/PeopleViewed.jsx
@@ -0,0 +1,30 @@
+
+import React from "react";
+
+import ListGroup from "react-bootstrap/ListGroup";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+
+class PeopleViewed extends React.Component {
+  render() {
+    return (
+     
+        <ListGroup>
+          <ListGroup.Item>Cras justo odio</ListGroup.Item>
+          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        </ListGroup>
+
+
+
+      
+    );
+  }
+}
+
+export default PeopleViewed;
