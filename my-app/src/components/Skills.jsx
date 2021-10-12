2b37ea613ead57609f39b3f0b01d932aa5cc5bc6
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-10-12 21:41:50 +0100

    Update Skills.jsx
    fetches array of users for list-group

--- my-app/src/components/Skills.jsx
+++ my-app/src/components/Skills.jsx
@@ -1,7 +1,34 @@
+import {useState, useEffect } from 'react'
 import { Card, Button, Row, Col, ListGroup } from "react-bootstrap";
 import { BsPencil } from "react-icons/bs";
 
 const Skills = () => {
+  
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+  
+  const fetchUsers = async () => {
+    try {
+      let response = await fetch('https://striveschool-api.herokuapp.com/api/profile/', {
+        headers: {
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTYzZTMxY2E4OTBjYzAwMTVjZjA3YzkiLCJpYXQiOjE2MzM5MzYxNTcsImV4cCI6MTYzNTE0NTc1N30.cQb5Rq2bVKtljqwRew41uKAJ7AUi3fQitiFeytDaAgQ' 
+        }
+      })
+      if (response.ok) {
+        let data = await response.json()
+        console.log(data)
+        setData(data)
+      } else {
+        console.log('Something went wrong with the PROFILE request')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <Card>
@@ -16,16 +43,21 @@ const Skills = () => {
 
                 <Button className="msgButton ml-2"><BsPencil /></Button>
               </Col>
-            <Col>
-            <Button className="msgButton" variant="outline-primary">Take Skill Quiz</Button>
-            </Col>
             </Row>
+            <div>
+            <Button className="msgButton" variant="outline-primary">Take Skill Quiz</Button>
+            </div>
             <p>
-            <span className="fw-bold">Teamwork</span> <span className="fw-light">10</span>
+            <span className="font-weight-bold">Teamwork</span> <span className="fw-light">10</span>
             </p>
           </Card.Body>
           <ListGroup variant="flush">
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            {
+              data.map((element) => (
+
+                <ListGroup.Item key={element._id}>{element.image}</ListGroup.Item>
+              ))
+            }
           </ListGroup>
           <Button variant="light" size="sm" className="QuizButton fw-bold">
             Show More
