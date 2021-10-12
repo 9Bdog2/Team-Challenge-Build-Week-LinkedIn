26de95d7236dfbee97530a25f33f52bb022023be
guillermoFragachan <guillefragachan@gmail.com>
2021-10-12 12:41:59 +0200

    posting working

--- my-app/src/components/ExperienceForm.jsx
+++ my-app/src/components/ExperienceForm.jsx
@@ -14,33 +14,41 @@ class ExperienceForm extends React.Component {
         }
   }
 
-
-
-  postExperience = async (query, exp) => {
+  handleInput = (key, value) =>{
       
+        this.setState({
+            addExperience: {
+                ...this.state.addExperience,
+                [key]:value,
+            }
+        })
+        
 
-    console.log(exp)
-    // try {
-    //   const response = await fetch(
-    //     "https://striveschool-api.herokuapp.com/api/profile/"+query+"/experiences",
-    //     {
-    //       method: "POST",
-    //       body: JSON.stringify(exp),
-    //       headers: new Headers({
-    //         "Content-Type": "application/json",
-    //         Authorization:
-    //           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTYzZTMxY2E4OTBjYzAwMTVjZjA3YzkiLCJpYXQiOjE2MzM5MzYxNTcsImV4cCI6MTYzNTE0NTc1N30.cQb5Rq2bVKtljqwRew41uKAJ7AUi3fQitiFeytDaAgQ",
-    //       }),
-    //     }
-    //   );
-    //   if (response.ok) {
-    //     this.fetchData();
-    //   } else {
-    //     console.log(response);
-    //   }
-    // } catch (e) {
-    //   console.log(e);
-    // }
+  }
+
+  postExperience = async (exp, query ='6163e31ca890cc0015cf07c9') => {
+      
+    try {
+      const response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/profile/"+query+"/experiences",
+        {
+          method: "POST",
+          body: JSON.stringify(exp),
+          headers: new Headers({
+            "Content-Type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTYzZTMxY2E4OTBjYzAwMTVjZjA3YzkiLCJpYXQiOjE2MzM5MzYxNTcsImV4cCI6MTYzNTE0NTc1N30.cQb5Rq2bVKtljqwRew41uKAJ7AUi3fQitiFeytDaAgQ",
+          }),
+        }
+      );
+      if (response.ok) {
+        console.log('posted')
+      } else {
+        console.log(response);
+      }
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   render() {
@@ -51,44 +59,54 @@ class ExperienceForm extends React.Component {
             
           <Form.Group id="Role" controlId="Role">
             <Form.Label>Role</Form.Label>
-            <Form.Control type="email" placeholder="QA Engineer" />
+            <Form.Control onChange={(e) => {
+                    this.handleInput("role", e.target.value);
+                  }} type="text" placeholder="QA Engineer" />
           </Form.Group>
 
           <Form.Group controlId="Company">
             <Form.Label>Company</Form.Label>
-            <Form.Control type="text" placeholder="Facebook inc." />
+            <Form.Control onChange={(e) => {
+                    this.handleInput("company", e.target.value);
+                  }} type="text" placeholder="Facebook inc." />
           </Form.Group>
 
           <Form.Group controlId="Start">
             <Form.Label>Start date</Form.Label>
-            <Form.Control type="date"  />
+            <Form.Control onChange={(e) => {
+                    this.handleInput("startDate", e.target.value);
+                  }} type="date"  />
           </Form.Group>
 
 
 
           <Form.Group controlId="End">
             <Form.Label>End Date</Form.Label>
-            <Form.Control type="date"  />
+            <Form.Control onChange={(e) => {
+                    this.handleInput("endDate", e.target.value);
+                  }} type="date"  />
           </Form.Group>
 
           
           <Form.Group controlId="Description">
             <Form.Label>Description</Form.Label>
-            <Form.Control type="text" placeholder="Changing vanilla JS to React components" />
+            <Form.Control onChange={(e) => {
+                    this.handleInput("description", e.target.value);
+                  }} type="text" placeholder="Changing vanilla JS to React components" />
           </Form.Group>
 
 
           <Form.Group controlId="Area">
             <Form.Label>Area</Form.Label>
-            <Form.Control type="text" placeholder="SA, California" />
+            <Form.Control onChange={(e) => {
+                    this.handleInput("area", e.target.value);
+                  }} type="text" placeholder="SA, California" />
           </Form.Group>
           
 
           
-          <Button variant="primary"  onClick={(event)=>{
-                this.postExperience(event, {
-                    role: null
-                })
+          <Button variant="primary"  onClick={()=>{
+                this.postExperience(this.state.addExperience)
           }}>
             Save
           </Button>
