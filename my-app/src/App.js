04b17c19230f5969c3edec4fed3fd60d4d92b7c4
Bogdan Birau <birau.bogdan92@gmail.com>
2021-10-12 14:25:41 +0100

    Added react-router-dom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,58 +1,41 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import TopNavbar from './components/TopNavbar'
-import BottomFooter from './components/BottomFooter'
-
+import TopNavbar from "./components/TopNavbar";
+import BottomFooter from "./components/BottomFooter";
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Sidebar from './components/SideBar'
-import './App.css'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Profilejumbotron from './components/Profilejumbotron'
-import { Button } from "react-bootstrap";
-import ExperienceLI from './components/ExperienceLI'
-import Skills from './components/Skills'
-import Activity from './components/Activity'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Sidebar from "./components/SideBar";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Profilejumbotron from "./components/Profilejumbotron";
+import ExperienceLI from "./components/ExperienceLI";
+import Skills from "./components/Skills";
+import Activity from "./components/Activity";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-
     <>
-    <TopNavbar/>
-    
-    <Container  >
-  <Row>
-    <Col md={8}>
+      <TopNavbar />
+
+      <Container>
+        <Row>
+          <Col md={8}>
+            <Profilejumbotron />
+            <Activity />
+            <ExperienceLI />
+            <Skills />
+          </Col>
+          <Col md={4}>
+            <Sidebar />
+          </Col>
+        </Row>
+      </Container>
       
-<Profilejumbotron/>
-<Activity/>
-<ExperienceLI/>
-<Skills/>
-
-
-
-    </Col>
-    <Col md={4} >
-      
-    <Sidebar/>
-
-    </Col>
-  </Row>
- 
-</Container>
-<BottomFooter/>
-{/* 
-<BottomFooter/> */}
-
-
-
-</>
+      <BottomFooter />
+    </>
   );
 }
 
-
 export default App;
-
-
