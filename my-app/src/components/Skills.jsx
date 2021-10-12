d1e5c8078bb274af139a2c29d840e9242314a9bc
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-10-12 15:40:54 +0100

    Update skills
    Skills and endorsements section

--- my-app/src/components/Skills.jsx
+++ my-app/src/components/Skills.jsx
@@ -1,29 +1,34 @@
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Row, Col, ListGroup } from "react-bootstrap";
+import { BsPencil } from "react-icons/bs";
 
 const Skills = () => {
   return (
     <>
       <Card>
         <Card className="Skills p-2">
-          <Card.Body className=" " style={{ fontSize: "20px" }}>
-            Skills and endorsements
-            <Button
-              variant="outline-secondary "
-              size="sm"
-              className=""
-              style={{ marginLeft: "73%", marginBottom: "20px" }}
-            >
-              See all activity
-            </Button>{" "}
-            <i class="fas fa-pen plus-icon p-2 ml-2 "></i>
+          <Card.Body>
+            <Row>
+              <Col md={8}>
+                <h4>Skills & endorsements</h4>
+              </Col>
+              <Col md={4}>
+                <a href="">Add new skill</a>
+
+                <Button className="msgButton ml-2"><BsPencil /></Button>
+              </Col>
+            <Col>
+            <Button className="msgButton" variant="outline-primary">Take Skill Quiz</Button>
+            </Col>
+            </Row>
+            <p>
+            <span className="fw-bold">Teamwork</span> <span className="fw-light">10</span>
+            </p>
           </Card.Body>
-          <Button
-            variant="outline-primary"
-            size="sm"
-            className="QuizButton"
-            style={{ marginRight: "75%" }}
-          >
-            See all activity
+          <ListGroup variant="flush">
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+          </ListGroup>
+          <Button variant="light" size="sm" className="QuizButton fw-bold">
+            Show More
           </Button>{" "}
         </Card>
       </Card>
