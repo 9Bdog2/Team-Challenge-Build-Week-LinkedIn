70e842b9765a51aaf23ad56dbbdc5a66177b0656
Bogdan Birau <birau.bogdan92@gmail.com>
2021-10-12 10:17:12 +0100

    Hardcoded Content

--- my-app/src/components/Interests.jsx
+++ my-app/src/components/Interests.jsx
@@ -1,28 +1,117 @@
-import { Card, Button } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import "./Interests.css";
 
+const imgUrl =
+  "https://media-exp1.licdn.com/dms/image/C5607AQGBY2IkkqVDqg/group-logo_image-shrink_200x200/0/1631008020913?e=1634112000&v=beta&t=Wmr4cV8r3GhnKD3wh9vuRsqWJF0ETj8FYbbtstnoSis";
 
-function Interests () {
-    return(
-        <>
-        <Card>
+const companies = [
+  "Copenhagen Fintech",
+  "Community Guidelines",
+  "Privacy & Terms",
+  "Safety Center",
+];
+
+function Interests() {
+  return (
+    <>
+      <Card>
         <Card className="Skills p-2">
           <Card.Body className=" " style={{ fontSize: "20px" }}>
             Interests
             <i class="fas fa-pen plus-icon p-2 ml-2 "></i>
           </Card.Body>
-          <Button
-            variant="outline-primary"
-            size="sm"
-            className="QuizButton"
-            style={{ marginRight: "75%" }}
-          >
-            See all activity
-          </Button>{" "}
+          <Card.Body>
+            <Container>
+              <Row>
+                <Col md={6}>
+                  <div className="interests_container">
+                    <img src={imgUrl} alt="fintech" style={{borderRadius: "0%"}}/>
+                    <div className="ml-3">
+                      <h3 className=" t-14 t-black t-bold ">
+                        <span className="companies">Copenhagen Fintech</span>
+                      </h3>
+                      <div>
+                        <p className="members">4,182 members</p>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col md={6}>
+                  <div className="interests_container">
+                    <img src={imgUrl} alt="fintech" style={{borderRadius: "0%"}}/>
+                    <div className="ml-3">
+                      <h3 className=" t-14 t-black t-bold ">
+                        <span className="companies">Copenhagen Fintech</span>
+                      </h3>
+                      <div>
+                        <p className="members">4,182 members</p>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col md={6}>
+                  <div className="interests_container">
+                    <img src={imgUrl} alt="fintech" style={{borderRadius: "0%"}}/>
+                    <div className="ml-3">
+                      <h3 className=" t-14 t-black t-bold ">
+                        <span className="companies">Copenhagen Fintech</span>
+                      </h3>
+                      <div>
+                        <p className="members">4,182 members</p>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col md={6}>
+                  <div className="interests_container">
+                    <img src={imgUrl} alt="fintech" style={{borderRadius: "0%"}}/>
+                    <div className="ml-3">
+                      <h3 className=" t-14 t-black t-bold ">
+                        <span className="companies">Copenhagen Fintech</span>
+                      </h3>
+                      <div>
+                        <p className="members">4,182 members</p>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col md={6}>
+                  <div className="interests_container">
+                    <img src={imgUrl} alt="fintech" style={{borderRadius: "0%"}}/>
+                    <div className="ml-3">
+                      <h3 className=" t-14 t-black t-bold ">
+                        <span className="companies">Copenhagen Fintech</span>
+                      </h3>
+                      <div>
+                        <p className="members">4,182 members</p>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col md={6}>
+                  <div className="interests_container">
+                    <img src={imgUrl} alt="fintech" style={{borderRadius: "0%"}} />
+                    <div className="ml-3">
+                      <h3 className=" t-14 t-black t-bold ">
+                        <span className="companies">Copenhagen Fintech</span>
+                      </h3>
+                      <div>
+                        <p className="members">4,182 members</p>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </Card.Body>
+          <div className="p_container">
+            <p className="bottom_p"> See all </p>
+          </div>
         </Card>
       </Card>
-        </>
-    )
-
+    </>
+  );
 }
 
-export default Interests
+export default Interests;
