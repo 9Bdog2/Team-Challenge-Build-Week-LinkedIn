5eeef062867aba41575e6a93943b535498ba9916
guillermoFragachan <guillefragachan@gmail.com>
2021-10-12 09:52:20 +0200

    css

--- my-app/src/components/ExperienceLI.jsx
+++ my-app/src/components/ExperienceLI.jsx
@@ -1,27 +1,93 @@
-import { Card } from "react-bootstrap";
+import React from "react";
 
-const ExperienceLI = () => {
-  return (
-    <>
-      <Card>
-        <Card className="ExperienceLI p-2">
-          <Card.Body className=" " style={{ fontSize: "20px" }}>
-            Experience
-            <i
-              class="fas fa-plus plus-icon p-2 "
-              style={{ marginLeft: "75%" }}
-            ></i>
-          </Card.Body>
+import ListGroup from 'react-bootstrap/ListGroup'
+import './ExperienceLI.css'
 
-          <img
-            src="https://icon-library.com/images/profile-icon-white/profile-icon-white-26.jpg"
-            alt="profile"
-            style={{ width: "45px" }}
-            className="ml-4"
-          />
-        </Card>
-      </Card>
-    </>
-  );
-};
+class ExperienceLI extends React.Component {
+
+    state={
+      fetchedExperience: [],
+
+    }
+
+
+
+
+  fetchData = async (query) =>{
+    const url = 'https://striveschool-api.herokuapp.com/api/profile/'+query+'/experiences'
+    try{
+        let response = await fetch(url, {
+            method: 'GET',
+            headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY1M2RjMmEwMjlmNTAwMTU3YzVhYzEiLCJpYXQiOjE2MzQwMjQ4OTgsImV4cCI6MTYzNTIzNDQ5OH0.2KFhYiMME1l87hLbFrmt3vhJkFLswovXieLuT8OMPbE",
+        'Content-Type': 'application/json',
+    }
+        }
+        )
+        if(response.ok){
+            let data = await response.json()
+        
+            console.log(data)
+            this.setState({
+              fetchedExperience: data
+                
+            })
+           
+        }else{
+            console.log("error");
+    }
+
+    }
+    catch (error) {
+        console.log(error);
+      }
+
+}
+
+  componentDidMount = () => {
+    this.fetchData('5d925e677360c41e0046d1f5')
+  }  
+  render(){
+    return(
+
+      <div className="mainBox">
+      <h6>{this.props.title}
+      </h6>
+      <ListGroup >
+          {
+          
+             
+                  this.state.fetchedExperience.length>0  &&
+              this.state.fetchedExperience.map(e=>{
+                  return <ListGroup.Item className="listElement" key={e._id}>
+                     <a className={'d-flex'}> 
+                     <img src={e.image} />
+                      
+
+                      <p className='name'><span >{e.name + e.surname  }</span>
+                      <p>{e.title} </p>
+                      </p>
+                     </a>
+                  
+                      
+                     
+                      
+                      
+                  </ListGroup.Item>
+              })
+
+              
+              
+       
+           }
+
+
+      </ListGroup>
+
+           </div>
+
+    )}
+
+}
+  
 export default ExperienceLI;
