0ef0276bb7324b9b9b671c71c86f72c005493357
guillermoFragachan <guillefragachan@gmail.com>
2021-10-13 10:53:08 +0200

    d

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -24,6 +24,7 @@ const ProfileView = () => (
         <Profilejumbotron />
         <Activity />
         <ExperienceLI />
+        <Interests/>
         <Skills />
       </Col>
       <Col md={4}>
