6ce87547b2ad13255c394acd8f3c3f6a319e0e1a
Bogdan Birau <birau.bogdan92@gmail.com>
2021-10-14 15:40:47 +0100

    Tetiana's solution

--- my-app/src/components/PostModal.jsx
+++ my-app/src/components/PostModal.jsx
@@ -5,39 +5,57 @@ import ImageUploader from "react-images-upload";
 
 class PostModal extends React.Component {
   state = {
-    text: {
-      text: null,
-    },
-    experienceImage: [null],
-    imageUpload: [null],
-    imageUploadingLoader: false,
+    image: null, //to set image when file is selected
+    body: {
+      // to send when create the post (to create the post you send teaxt only!)
+      text: ""
+    }
   };
 
   hideModal = () => {
     this.props.setShow(false);
   };
 
-  postData = async (postText) => {
+  postData = async () => {
     let formData = new FormData();
-    let blob = new Blob([this.state.imageUpload[0]], { type: "img/jpeg" });
-    formData.append("experience", blob);
+    formData.append("post", this.state.image); // "post" is the name of the file you send to post an image. (Specified on eduflow!)
 
     try {
+      // First request to create a post
+
       const response = await fetch(
         "https://striveschool-api.herokuapp.com/api/posts/",
         {
           method: "POST",
-          body: formData,
-          body: JSON.stringify(postText),
+          body: JSON.stringify(this.state.body), // I send only text in the body
           headers: new Headers({
             "Content-Type": "application/json",
             Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTYzZTMxY2E4OTBjYzAwMTVjZjA3YzkiLCJpYXQiOjE2MzM5MzYxNTcsImV4cCI6MTYzNTE0NTc1N30.cQb5Rq2bVKtljqwRew41uKAJ7AUi3fQitiFeytDaAgQ",
-          }),
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTYzZTMxY2E4OTBjYzAwMTVjZjA3YzkiLCJpYXQiOjE2MzM5MzYxNTcsImV4cCI6MTYzNTE0NTc1N30.cQb5Rq2bVKtljqwRew41uKAJ7AUi3fQitiFeytDaAgQ"
+          })
         }
       );
       if (response.ok) {
-        console.log("posted");
+        alert("posted");
+        const post = await response.json(); // I need post id from created post in order to send a second request to post image
+
+        // Sending second request to post image
+
+        const imgUploadResponse = await fetch(
+          `https://striveschool-api.herokuapp.com/api/posts/${post._id}`,
+          {
+            body: formData, // in the body sending only formData with  file inside
+            method: "POST",
+            headers: new Headers({
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTYzZTMxY2E4OTBjYzAwMTVjZjA3YzkiLCJpYXQiOjE2MzM5MzYxNTcsImV4cCI6MTYzNTE0NTc1N30.cQb5Rq2bVKtljqwRew41uKAJ7AUi3fQitiFeytDaAgQ"
+            })
+          }
+        );
+        console.log(imgUploadResponse);
+        if (imgUploadResponse.ok) {
+          alert("uploaded");
+        }
       } else {
         console.log(response);
       }
@@ -46,9 +64,8 @@ class PostModal extends React.Component {
     }
   };
 
-  componentDidUpdate = (prevProps) => {};
-
   render() {
+    console.log(this.state);
     return (
       <Modal
         size="lg"
@@ -70,15 +87,17 @@ class PostModal extends React.Component {
           <Form>
             <Form.Group controlId="exampleForm.ControlTextarea1">
               <Form.Control
+                value={this.state.body.text}
                 className="form"
                 placeholder="What do you want to talk about?"
                 as="textarea"
                 rows={3}
                 onChange={(e) => {
                   this.setState({
-                    text: {
-                      text: e.target.value,
-                    },
+                    //properly seting text value on change
+                    body: {
+                      text: e.target.value
+                    }
                   });
                 }}
               />
@@ -91,29 +110,8 @@ class PostModal extends React.Component {
                 singleImage={true}
                 withPreview={true}
                 withLabel={false}
-                onChange={this.profilePictureUploadHandler}
+                onChange={(files) => this.setState({ image: files[0] })} // properly seting fie to the state when it is selected
               />
-              <div
-                className="d-flex justify-content-center align-items-center"
-                style={{ height: 40 }}
-              >
-                <Button
-                  variant="outline-secondary"
-                  className="rounded-pill mr-2"
-                  onClick={this.openUploadWindowHandler}
-                  style={{ width: "40%" }}
-                >
-                  Cancel
-                </Button>
-                <Button
-                  variant="primary"
-                  className="rounded-pill"
-                  style={{ width: "60%" }}
-                  onClick={this.postProfilePictureHandler}
-                >
-                  Save Changes
-                </Button>
-              </div>
             </Form.Group>
           </Form>
           <Button
